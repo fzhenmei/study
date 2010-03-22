49025782a186e2ec1a89d72c327f750124a19c76
chris.zen <fzhenmei@gmial.com>
2010-03-22 15:46:41 +0800

    add event bubble sample

---
+++ Silverlight/SilverlightBasic/EventBubble/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace EventBubble
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Block1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Block1.Text += "Block";
+        }
+
+        private void Canvas1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Block1.Text += " Canvas";
+        }
+
+        private void LayoutRoot_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Block1.Text += " Root";
+        }
+    }
+}
