b50f83a5ab607bc60635b6a5495c4d63af84fbad
chris.zen <fzhenmei@gmial.com>
2010-03-25 17:13:03 +0800

    accordion drag and drop...

--- Silverlight/DragDropLearn/DragDropLearn/MainPage.xaml.cs
+++ Silverlight/DragDropLearn/DragDropLearn/MainPage.xaml.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
+﻿using System.Collections.Generic;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using SL_Drag_Drop_BaseClasses;
 
 namespace DragDropLearn
--- Silverlight/MenuDemo/SilverlightMenu/MainPage.xaml.cs
+++ Silverlight/MenuDemo/SilverlightMenu/MainPage.xaml.cs
@@ -13,31 +13,32 @@ namespace SilverlightMenu
         public MainPage()
         {
             InitializeComponent();
+            InitialValues.ContainingLayoutPanel = LayoutRoot;
             this.Loaded += new RoutedEventHandler(MainPage_Loaded);
         }
 
         void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
             Loading.IsBusy = true;
-            //var srv = new MenuDemoClient();
-            //srv.GetAllMenuNodesCompleted += new EventHandler<GetAllMenuNodesCompletedEventArgs>(srv_GetAllMenuNodesCompleted);
-            //srv.GetAllMenuNodesAsync();
+            var srv = new MenuDemoClient();
+            srv.GetAllMenuNodesCompleted += new EventHandler<GetAllMenuNodesCompletedEventArgs>(srv_GetAllMenuNodesCompleted);
+            srv.GetAllMenuNodesAsync();
             //init drop & drag
-            InitDragDrop();
+            //InitDragDrop();
         }
 
-        private void InitDragDrop()
-        {
-            var target1 = new DropTarget() { Width = 100, Height = 20 };
-            var target2 = new DropTarget() { Width = 100, Height = 20 };
-            PanelDropTargets.Children.Add(target1);
-            PanelDropTargets.Children.Add(target2);
-            var targets = new List<DropTarget>() { target1, target2 };
-            var dragSouce1 = new DragSource() { Content = new HyperlinkButton() { Content = "Test Source 1" }, DropTargets = targets, Ghost = new HyperlinkButton()};
-            var dragSouce2 = new DragSource() { Content = new HyperlinkButton() { Content = "Test Source 2" }, DropTargets = targets, Ghost = new HyperlinkButton()};
-            PanelDropSources.Children.Add(dragSouce1);
-            PanelDropSources.Children.Add(dragSouce2);
-        }
+        //private void InitDragDrop()
+        //{
+        //    var target1 = new DropTarget() { Width = 100, Height = 20 };
+        //    var target2 = new DropTarget() { Width = 100, Height = 20 };
+        //    PanelDropTargets.Children.Add(target1);
+        //    PanelDropTargets.Children.Add(target2);
+        //    var targets = new List<DropTarget>() { target1, target2 };
+        //    var dragSouce1 = new DragSource() { Content = new HyperlinkButton() { Content = "Test Source 1" }, DropTargets = targets, Ghost = new HyperlinkButton()};
+        //    var dragSouce2 = new DragSource() { Content = new HyperlinkButton() { Content = "Test Source 2" }, DropTargets = targets, Ghost = new HyperlinkButton()};
+        //    PanelDropSources.Children.Add(dragSouce1);
+        //    PanelDropSources.Children.Add(dragSouce2);
+        //}
 
         void srv_GetAllMenuNodesCompleted(object sender, GetAllMenuNodesCompletedEventArgs e)
         {
@@ -46,31 +47,45 @@ namespace SilverlightMenu
                                        {
                                            var topNodes = data.Where(n => n.ParentId == 0);
                                            var dropTagets = new List<DropTarget>();
+                                           var dragSource = new List<DragSource>();
+
                                            foreach (var node in topNodes)
                                            {
                                                //todo: add dragtarget for header...
                                                var target = new DropTarget
-                                                                       {
-                                                                           Width = 100,
-                                                                           Height = 30,
-                                                                       };
-                                               var source = new DragSource() { Content = new HyperlinkButton() { Content = node.NodeName } };
-                                               source.DropTargets.Add(target);
-                                               target.Content = source;
-
+                                               {
+                                                   Content = new DragSource() { Content = new HyperlinkButton { Content = node.NodeName}, DraggingEnabled = false},
+                                                   Width = 200,
+                                                   Height = 30,
+                                               };
                                                target.DragSourceDropped += new DropEventHandler(target_DragSourceDropped);
+                                               dropTagets.Add(target);
+
                                                var item = new AccordionItem { Header = target };
 
                                                var currentNode = node;
                                                var children = data.Where(d => d.ParentId == currentNode.Id).ToList();
                                                if (children.Count() > 0)
                                                {
-                                                   //todo: add dragtarget for content
-                                                   //todo: add dragsource for content
-                                                   var wrapper = new StackPanel();
+                                                   var wrapper = new StackPanel();//this is a container,should have targets
                                                    foreach (var child in children)
                                                    {
-                                                       wrapper.Children.Add(new HyperlinkButton() { Content = child.NodeName });
+                                                       var childTarget = new DropTarget()
+                                                                             {
+                                                                                 Ghost =
+                                                                                     new HyperlinkButton() { Content = child.NodeName },
+                                                                             };
+                                                       var source = new DragSource()
+                                                                        {
+                                                                            Content = new HyperlinkButton() { Content = child.NodeName, Width = 100, Height = 50},
+                                                                            //DragHandleMode = DragSource.DragHandleModeType.FullDragSource
+                                                                        };
+                                                       
+                                                       dragSource.Add(source);
+                                                       childTarget.Content = source;
+                                                       dropTagets.Add(childTarget);
+
+                                                       wrapper.Children.Add(childTarget);
                                                    }
                                                    item.Content = wrapper;
                                                }
@@ -81,6 +96,11 @@ namespace SilverlightMenu
 
                                                LeftMenu.Items.Add(item);
                                            }
+
+                                           for (int i = 0; i < dragSource.Count; i++)
+                                           {
+                                               dragSource[i].DropTargets = dropTagets;
+                                           }
                                        });
             Loading.IsBusy = false;
             Loading.Visibility = System.Windows.Visibility.Collapsed;
@@ -88,7 +108,10 @@ namespace SilverlightMenu
 
         void target_DragSourceDropped(object sender, DropEventArgs args)
         {
-            //throw new NotImplementedException();
+            if (args.DragSource.Tag.ToString() == "Test")
+            {
+                
+            }
         }
     }
 }
