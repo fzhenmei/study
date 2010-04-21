47be287cdf2255ef5d70f5d395ab1d8780bda5bb
chris.zen <fzhenmei@gmial.com>
2010-04-21 15:32:49 +0800

    add assemblyinfo.cs

---
+++ NCrawler/Net 4.0/NCrawler.SQLite/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NCrawler.SQLite")]
+[assembly: AssemblyDescription("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("430bbcbc-c918-4af8-9ecc-862a7165fa99")]
