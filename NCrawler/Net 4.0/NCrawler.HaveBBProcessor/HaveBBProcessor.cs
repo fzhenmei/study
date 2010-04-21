46f62dd943eb5e7cdf6c76695848a29b183c8a39
chris.zen <fzhenmei@gmail.com>
2010-04-21 22:04:30 +0800

    add new project

---
+++ NCrawler/Net 4.0/NCrawler.HaveBBProcessor/HaveBBProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NCrawler.Interfaces;
+
+namespace NCrawler.HaveBBProcessor
+{
+    public class HaveBBProcessor : IPipelineStep
+    {
+        public void Process(Crawler crawler, PropertyBag propertyBag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
