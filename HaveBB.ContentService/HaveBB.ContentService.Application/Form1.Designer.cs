cdb31964b95d066184affb9d825088ee982c8200
chris.zen <fzhenmei@gmail.com>
2010-07-19 22:42:15 +0800

    add content service demo

---
+++ HaveBB.ContentService/HaveBB.ContentService.Application/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace HaveBB.ContentService.App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStartUrl = new System.Windows.Forms.TextBox();
+            this.rtbEcho = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(344, 322);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(425, 322);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "开始地址：";
+            // 
+            // txtStartUrl
+            // 
+            this.txtStartUrl.Location = new System.Drawing.Point(109, 17);
+            this.txtStartUrl.Name = "txtStartUrl";
+            this.txtStartUrl.Size = new System.Drawing.Size(647, 21);
+            this.txtStartUrl.TabIndex = 3;
+            // 
+            // rtbEcho
+            // 
+            this.rtbEcho.Location = new System.Drawing.Point(109, 55);
+            this.rtbEcho.Name = "rtbEcho";
+            this.rtbEcho.Size = new System.Drawing.Size(647, 249);
+            this.rtbEcho.TabIndex = 4;
+            this.rtbEcho.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 357);
+            this.Controls.Add(this.rtbEcho);
+            this.Controls.Add(this.txtStartUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStartUrl;
+        private System.Windows.Forms.RichTextBox rtbEcho;
+    }
+}
+
---
+++ HaveBB.ContentService/HaveBB.ContentService.Application/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using NCrawler;
+using NCrawler.Extensions;
+using NCrawler.HtmlProcessor;
+using NCrawler.Interfaces;
+using NCrawler.Services;
+
+namespace HaveBB.ContentService.App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var uri = new Uri(txtStartUrl.Text.Trim());
+
+            ServicePointManager.MaxServicePoints = 999999;
+            ServicePointManager.DefaultConnectionLimit = 999999;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+            ServicePointManager.CheckCertificateRevocationList = true;
+            ServicePointManager.EnableDnsRoundRobin = true;
+
+            using (var c = new Crawler(
+                uri, 
+                new HtmlDocumentProcessor(),
+                new EchoStep(rtbEcho)
+                ))
+            {
+                c.MaximumThreadCount = 3;
+                c.MaximumCrawlDepth = 10;
+                c.ExcludeFilter = new[]
+                                      {
+                                          new RegexFilter(
+                                              new Regex(@"(\.jpg|\.css|\.js|\.gif|\.jpeg|\.png|\.ico)",
+                                                        RegexOptions.Compiled | RegexOptions.CultureInvariant |
+                                                        RegexOptions.IgnoreCase))
+                                      };
+                c.Crawl();
+            }
+        }
+
+        internal class EchoStep : IPipelineStep
+        {
+            public EchoStep(RichTextBox echoControl)
+            {
+                EchoControl = echoControl;
+            }
+
+            protected RichTextBox EchoControl { get; set; }
+
+            #region IPipelineStep Members
+
+            /// <summary>
+            /// </summary>
+            /// <param name="crawler">
+            /// The crawler.
+            /// </param>
+            /// <param name="propertyBag">
+            /// The property bag.
+            /// </param>
+            public void Process(Crawler crawler, PropertyBag propertyBag)
+            {
+                //CultureInfo contentCulture = (CultureInfo)propertyBag["LanguageCulture"].Value;
+                //string cultureDisplayValue = "N/A";
+                //if (!contentCulture.IsNull())
+                //{
+                //    cultureDisplayValue = contentCulture.DisplayName;
+                //}
+
+                lock (this)
+                {
+                    EchoControl.Text += propertyBag.Title;
+                    EchoControl.Text += "\r\n";
+
+                    //Console.Out.WriteLine(ConsoleColor.Gray, "Url: {0}", propertyBag.Step.Uri);
+                    //Console.Out.WriteLine(ConsoleColor.DarkGreen, "\tContent type: {0}", propertyBag.ContentType);
+                    //Console.Out.WriteLine(ConsoleColor.DarkGreen, "\tContent length: {0}", propertyBag.Text.IsNull() ? 0 : propertyBag.Text.Length);
+                    //Console.Out.WriteLine(ConsoleColor.DarkGreen, "\tDepth: {0}", propertyBag.Step.Depth);
+                    //Console.Out.WriteLine(ConsoleColor.DarkGreen, "\tCulture: {0}", cultureDisplayValue);
+                    //Console.Out.WriteLine(ConsoleColor.DarkGreen, "\tThreadId: {0}", System.Threading.Thread.CurrentThread.ManagedThreadId);
+                    //Console.Out.WriteLine(ConsoleColor.DarkGreen, "\tThread Count: {0}", crawler.ThreadsInUse);
+                    //Console.Out.WriteLine();
+                }
+            }
+
+            #endregion
+        }
+    }
+}
